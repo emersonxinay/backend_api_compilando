463463e61e8745e1ceda42aa05cdb4b2759e2822
emerson <xinayespinoza@gmail.com>
2024-11-10 17:57:55 -0300

    feat app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+# Habilitar CORS para que React pueda hacer peticiones
+CORS(app)
+
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        "message": "¡Hola desde Flask!"
+    }
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
